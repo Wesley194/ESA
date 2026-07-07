a7c5a0ff696ea732749ff5b963d3ad3f4f595ca7
Wesley194 <Ostrich238554723@gmail.com>
2026-07-08 03:48:17 +0800

    add agent(Q-learning)

--- esa_main_framework.py
+++ esa_main_framework.py
@@ -10,11 +10,33 @@ from esa_sampling_actions import a1_de_screening, a2_surrogate_local_search, a3_
 #import LHS抽樣
 
 # RL Agent(Q-Learning)
-class QAgent:
-    """
-    填入程式碼
-    主迴圈裡面有預先定義一些method名稱，要注意一下有沒有相符
-    """
+class QAgent():
+    """ 透過 RL(Q-learning) 決定要採取哪種策略 """
+    def __init__(self):
+        self.Q_table = np.full((8, 4), 0.25) 
+        self.gamma = 0.9
+        self.learning_rate = 0.1 # learning_rate(alpha)
+        self.current_state = 0 # 初始狀態設為 0 (s1)
+
+    def select_action(self):
+        """ 實作 softmax (公式10)，並用輪盤法作出最終選擇 """
+        # softmax
+        Q_values = self.Q_table[self.current_state, :]
+        exp_Q = np.exp(Q_values)
+        probabilities = exp_Q / np.sum(exp_Q)
+
+        # 輪盤法
+        action = np.random.choice(a=[0, 1, 2, 3], p=probabilities)
+        return action
+    
+    def update(self, action, reward, improved):
+        """ 實作公式 6 """
+        success = 1 if improved else 0
+        next_state = 2 * action + success
+        max_q_nex = np.max(self.Q_table[next_state, :])
+        self.Q_table[self.current_state][action] = (1 - self.learning_rate) * self.Q_table[self.current_state][action] + self.learning_rate * (reward + self.gamma * max_q_nex)
+
+        self.current_state = next_state
 
 # 目標函數設定
 def real_objective_function(x: np.ndarray) -> float:
